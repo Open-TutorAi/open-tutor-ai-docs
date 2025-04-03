9f9fd9d4d170532afc80b271fa4a22002ea5a28e
tarekai <tr.aitbaha@gmail.com>
2025-04-03 21:11:37 +0000

    ghpage configuration update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,14 +11,14 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://tarekai.github.io',
+  url: 'http://opentutorai.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/open-tutor-ai-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'tarekai', // Usually your GitHub org/user name.
+  organizationName: 'pr-elhajji', // Usually your GitHub org/user name.
   projectName: 'open-tutor-ai-docs', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   onBrokenLinks: 'ignore',
