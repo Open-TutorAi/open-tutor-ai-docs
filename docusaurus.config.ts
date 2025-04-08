cf7051f26a0516f1b432e103f2a0daa5df713cfe
Mohamed El hajji <54247940+pr-elhajji@users.noreply.github.com>
2025-04-08 13:10:17 +0100

    Update docusaurus.config.ts (#10)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,10 +11,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'opentutorai.com',
+  url: 'https://opentutorai.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/open-tutor-ai-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
